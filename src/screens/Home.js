c96e717d295bdd55d6d4a0bb6e852e1fd5da237e
sameerjkl <sameerjakkali@gmail.com>
2024-03-26 20:07:39 +0530

    updated code

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,50 +1,94 @@
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Switch, Button, Alert } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Switch,
+  Button,
+  Alert,
+  Picker,
+} from "react-native";
+import { Clipboard } from "react-native-web";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function Home() {
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState("");
   const [characterCount, setCharacterCount] = useState(8);
   const [includeSymbols, setIncludeSymbols] = useState(false);
   const [includeUpperCase, setIncludeUpperCase] = useState(true);
   const [includeLowerCase, setIncludeLowerCase] = useState(true);
   const [includeCamelCase, setIncludeCamelCase] = useState(false);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
   const [lettersOnly, setLettersOnly] = useState(false);
   const [numbersOnly, setNumbersOnly] = useState(false);
-  const [prefixOrSuffix, setPrefixOrSuffix] = useState('');
-  const [specificCharacter, setSpecificCharacter] = useState('');
+  const [prefixOrSuffix, setPrefixOrSuffix] = useState("");
+  const [specificCharacter, setSpecificCharacter] = useState("");
 
   const generatePassword = () => {
-    let charset = '';
-    if (includeUpperCase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (includeLowerCase) charset += 'abcdefghijklmnopqrstuvwxyz';
-    if (includeSymbols) charset += '!@#$%^&*()_+-=[]{}|;:,.<>?';
-    if (includeCamelCase) charset += 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (numbersOnly) charset += '0123456789';
-    if (lettersOnly) charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    
-    let newPassword = '';
+    let charset = "";
+
+    // Include selected character sets
+    if (includeUpperCase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (includeLowerCase) charset += "abcdefghijklmnopqrstuvwxyz";
+    if (includeSymbols) charset += "!@#$%^&*()_+-=[]{}|;:,.<>?";
+    if (includeNumbers) charset += "0123456789";
+    if (includeCamelCase)
+      charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numbersOnly) charset = "0123456789";
+    if (lettersOnly)
+      charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (!charset) {
+      Alert.alert("Please select at least one character set.");
+      return;
+    }
+
+    let newPassword = "";
     for (let i = 0; i < characterCount; i++) {
       const randomIndex = Math.floor(Math.random() * charset.length);
       newPassword += charset[randomIndex];
     }
 
-    if (prefixOrSuffix === 'prefix') newPassword = specificCharacter + newPassword;
-    if (prefixOrSuffix === 'suffix') newPassword += specificCharacter;
+    // Add prefix or suffix if specified
+    if (prefixOrSuffix === "prefix")
+      newPassword = specificCharacter + newPassword;
+    else if (prefixOrSuffix === "suffix") newPassword += specificCharacter;
 
     setPassword(newPassword);
+  };
+  const copyToClipboard = () => {
+    Clipboard.setString(password);
+    Alert.alert("Password copied to clipboard");
   };
 
   return (
     <View style={styles.container}>
-      <Text>Password: {password}</Text>
+      <Text style={styles.passwordText}>{password}</Text>
+
+      <Button title="Copy to Clipboard" onPress={copyToClipboard} />
+      <Icon
+        name="copy" // Specify the icon name
+        size={24} // Adjust the size of the icon
+        color="#000" // Adjust the color of the icon
+        onPress={copyToClipboard} // Attach onPress event handler
+      />
+
       <TextInput
         style={styles.input}
-        onChangeText={text => setCharacterCount(parseInt(text))}
-        value={String(characterCount)}
+        onChangeText={(text) => {
+          if (!isNaN(parseInt(text))) {
+            setCharacterCount(parseInt(text));
+          } else if (text === "") {
+            setCharacterCount(0); // Or any default value you prefer
+          }
+        }}
+        value={characterCount === 0 ? "" : String(characterCount)}
         keyboardType="numeric"
         placeholder="Character Count"
       />
+
       <View style={styles.switchContainer}>
         <Text>Special Characters</Text>
         <Switch value={includeSymbols} onValueChange={setIncludeSymbols} />
@@ -53,14 +97,19 @@ export default function Home() {
         <Text>Upper Case</Text>
         <Switch value={includeUpperCase} onValueChange={setIncludeUpperCase} />
       </View>
+
       <View style={styles.switchContainer}>
         <Text>Lower Case</Text>
         <Switch value={includeLowerCase} onValueChange={setIncludeLowerCase} />
       </View>
       <View style={styles.switchContainer}>
+        <Text>Digits</Text>
+        <Switch value={includeNumbers} onValueChange={setIncludeNumbers} />
+      </View>
+      {/* <View style={styles.switchContainer}>
         <Text>Camel Case</Text>
         <Switch value={includeCamelCase} onValueChange={setIncludeCamelCase} />
-      </View>
+      </View> */}
       <View style={styles.switchContainer}>
         <Text>Letters Only</Text>
         <Switch value={lettersOnly} onValueChange={setLettersOnly} />
@@ -69,17 +118,21 @@ export default function Home() {
         <Text>Numbers Only</Text>
         <Switch value={numbersOnly} onValueChange={setNumbersOnly} />
       </View>
+      <Picker
+        style={styles.input}
+        selectedValue={prefixOrSuffix}
+        onValueChange={(itemValue) => setPrefixOrSuffix(itemValue)}
+      >
+        <Picker.Item label="None" value="" />
+        <Picker.Item label="Prefix" value="prefix" />
+        <Picker.Item label="Suffix" value="suffix" />
+      </Picker>
+
       <TextInput
         style={styles.input}
-        onChangeText={text => setPrefixOrSuffix(text.toLowerCase())}
-        value={prefixOrSuffix}
-        placeholder="Prefix or Suffix?"
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={text => setSpecificCharacter(text)}
+        onChangeText={(text) => setSpecificCharacter(text)}
         value={specificCharacter}
-        placeholder="Specific Character"
+        placeholder={prefixOrSuffix}
       />
       <Button title="Generate Password" onPress={generatePassword} />
       <StatusBar style="auto" />
@@ -90,28 +143,20 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   input: {
-    height: 40,
-    width: '80%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginVertical: 10,
-    paddingHorizontal: 10,
-  },
-  switchContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#f0f0f0',
-    padding: 10,
+    height: 50,
+    width: "80%",
+    backgroundColor: "#f0f0f0",
     borderRadius: 8,
-    marginBottom: 10,
-    width: '80%',
-    justifyContent: 'space-between',
-    shadowColor: '#000',
+    paddingHorizontal: 15,
+    marginBottom: 20,
+    fontSize: 16,
+    color: "#333",
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -119,6 +164,28 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-  }
-  ,
+  },
+  passwordText: {
+    fontSize: 24, // Adjust the font size as needed
+    textAlign: "center", // Center the text horizontally
+    marginTop: 20, // Add some top margin for spacing
+  },
+  switchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    padding: 10,
+    borderRadius: 8,
+    marginBottom: 10,
+    width: "80%",
+    justifyContent: "space-between",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
 });
